de43d06d1e790e179c5136dd67aec325e65ed464
matsuaki <matsuaki@es-4.eedept.kobe-u.ac.jp>
2023-05-30 04:57:16 +0900

    電気電子回路を分けた

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 insert into genre(gid,genre_name) values (1,'問題'),(2,'解答'),(3,'ノート');
-insert into subject(sid,subject_name) values (1,'数学'),(2,'電磁気学'),(3,'電気電子回路'),(4,'自動制御'),
-(5,'情報理論'),(6,'論理回路');
+insert into subject(sid,subject_name) values (1,'数学'),(2,'電磁気学'),(3,'電気回路'),(4,'電子回路'),
+(5,'自動制御'),(6,'情報理論'),(7,'論理回路');
